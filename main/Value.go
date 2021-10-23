59026fd77ccba362d927f53498222b6aedb45cd2
haogee <1969390664@qq.com>
2021-10-23 11:43:49 +0800

    commit

---
+++ main/Value.go
@@ -0,0 +1,125 @@
+package main
+
+import "fmt"
+
+type Value interface {
+	Len() int
+}
+
+type FAL struct {
+	Num  int64
+	Nume int64
+	Deno int64
+}
+
+type PositiveInt uint
+
+type Sign struct {
+	s byte
+}
+
+func (p PositiveInt) Len() int {
+	return 0
+}
+func (s FAL) Len() int {
+	return 0
+}
+func (s Sign) Len() int {
+	return 0
+}
+
+// Format output
+func (s FAL) String() string { // 格式化输出
+	if s.Nume == 0 {
+		return fmt.Sprintf("%v", s.Num)
+	}
+	if s.Num == 0 {
+		return fmt.Sprintf("%v/%v", s.Nume, s.Deno)
+	}
+	return fmt.Sprintf("%v'%v/%v", s.Num, s.Nume, s.Deno)
+}
+
+// Model Create a score (molecular, denominator) with a denominator default of 1
+func Model(nd ...int64) *FAL { // 创建一个分数(分子，分母)，分母默认为1
+	var f *FAL
+	f = new(FAL)
+	//fmt.Println("after:", nd)
+	f.Num = nd[0]
+	f.Nume = nd[1]
+	f.Deno = nd[2]
+
+	return f
+}
+
+// Broadsheet  .阔张
+func (s *FAL) broad(lcm int64) {
+	s.Nume = s.Nume * (lcm / s.Deno)
+	s.Deno = lcm
+}
+
+// Compression Finishing .压缩 整理
+func (s *FAL) offset() {
+	if s.Nume == 0 {
+		s.Deno = 1
+		return
+	}
+	lcm := Gcd(s.Nume, s.Deno)
+	s.Nume /= lcm
+	s.Deno /= lcm
+}
+
+// Add Fraction addition
+func (s *FAL) Add(f *FAL) *FAL {
+	// Getting the Minimum Common Multiplier 获取最小公倍数
+	lcm := Lcm(f.Deno, s.Deno)
+	s.broad(lcm)
+	f.broad(lcm)
+
+	s.Nume += f.Nume
+	s.offset()
+	return s
+}
+
+// Sub fraction subtraction
+func (s *FAL) Sub(f *FAL) *FAL {
+	// Getting the Minimum Common Multiplier 获取最小公倍数
+	lcm := Lcm(s.Deno, f.Deno)
+	s.broad(lcm)
+	f.broad(lcm)
+
+	s.Nume -= f.Nume
+	s.offset()
+	return s
+}
+
+// Mul multiplication
+func (s *FAL) Mul(f *FAL) *FAL { // 乘法
+	s.Deno *= f.Deno
+	s.Nume *= f.Nume
+	s.offset()
+	return s
+}
+
+// Div division
+func (s *FAL) Div(f *FAL) *FAL { // 除法
+	s.Mul(Model(f.Num, f.Deno, f.Nume))
+	s.offset()
+	return s
+}
+
+// Verdict Calculation results
+func (s *FAL) Verdict() float64 { // 计算结果
+	return float64(s.Nume) / float64(s.Deno)
+}
+
+func Gcd(x int64, y int64) int64 {
+	if y == 0 {
+		return x
+	} else {
+		return Gcd(y, x%y)
+	}
+}
+
+func Lcm(x int64, y int64) int64 {
+	return x / Gcd(x, y) * y
+}
---
+++ main/operate.go
@@ -0,0 +1,43 @@
+package main
+
+func fakefenshu(v *FAL) {
+	v.Nume += v.Num * v.Deno
+	v.Num = 0
+}
+
+func realfenshu(v *FAL) {
+	v.Num = v.Nume / v.Deno
+	v.Nume %= v.Deno
+}
+func Add(v1 *FAL, v2 *FAL) *FAL {
+	fakefenshu(v1)
+	fakefenshu(v2)
+	//fmt.Println(v1.Num, v1.Nume, v1.Deno)
+	v1.Add(v2)
+	realfenshu(v1)
+	return v1
+}
+
+func Sub(v1 *FAL, v2 *FAL) *FAL {
+	fakefenshu(v1)
+	fakefenshu(v2)
+	v1.Sub(v2)
+	realfenshu(v1)
+	return v1
+}
+
+func Mul(v1 *FAL, v2 *FAL) *FAL {
+	fakefenshu(v1)
+	fakefenshu(v2)
+	v1.Mul(v2)
+	realfenshu(v1)
+	return v1
+}
+
+func Div(v1 *FAL, v2 *FAL) *FAL {
+	fakefenshu(v1)
+	fakefenshu(v2)
+	v1.Div(v2)
+	realfenshu(v1)
+	return v1
+}
